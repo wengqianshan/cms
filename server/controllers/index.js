4b4bdb7623f8e95d1052fd5c76421f36031fe558
xiaoshan5733 <wengqianshan5733@gmail.com>
2014-06-11 01:40:12 +0800

    admin index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,13 +7,14 @@ var user = require('../../server/controllers/user');
 
 exports.index = function(req, res) {
     if(req.session.user) {
-        user.checkRole('admin', req.session.user._id, function(err, user) {
+        res.render('server/index', { title: 'CMS系统' });
+        /*user.checkRole('admin', req.session.user._id, function(err, user) {
             console.log('校验通过', user);
             res.render('server/index', { title: 'CMS系统' });
         }, function(err) {
             console.log(err);
             res.render('server/message', { msg: '你不是管理员' });
-        });
+        });*/
     }/*else{
         var path = util.translateAdminDir('/user/login');
         res.redirect(path);
