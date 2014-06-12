7d3bd47be502085a5147a58bf00d14bf101708b7
xiaoshan5733 <wengqianshan5733@gmail.com>
2014-06-13 01:26:15 +0800

    error handle

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('server/error', {
+        res.render('app/error', {
             message: err.message,
             error: err
         });
@@ -100,7 +100,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('server/error', {
+    res.render('app/error', {
         message: err.message,
         error: {}
     });
