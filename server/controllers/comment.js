a66b400dcbfb26488a61eb76745e39014d2b87c3
xiaoshan5733 <wengqianshan5733@gmail.com>
2014-06-25 15:47:06 +0800

    admin/comment

---
+++ server/controllers/comment.js
@@ -0,0 +1,78 @@
+'use strict';
+var mongoose = require('mongoose'),
+    Comment = mongoose.model('Comment'),
+    util = require('../libs/util');
+//列表
+exports.list = function(req, res) {
+    var condition = {};
+    if(req.Roles && req.Roles.indexOf('admin') < 0) {
+        condition.author = req.session.user._id;
+    }
+    Comment.count(condition, function(err, total) {
+        var query = Comment.find(condition).populate('author').populate('from');
+        //分页
+        var pageInfo = util.createPage(req, total, 10);
+        //console.log(pageInfo);
+        query.skip(pageInfo.start);
+        query.limit(pageInfo.pageSize);
+        query.sort({created: -1});
+        query.exec(function(err, results) {
+            //console.log(err, results);
+            res.render('server/comment/list', {
+                //title: '列表',
+                comments: results,
+                pageInfo: pageInfo
+            });
+        })
+    })
+    
+};
+//单条
+exports.one = function(req, res) {
+    var id = req.param('id');
+    Comment.findById(id).populate('author', 'username name email').exec(function(err, result) {
+        console.log(result);
+        if(!result) {
+            return res.render('server/message', {
+                msg: '该分类不存在'
+            });
+        }
+        res.render('server/comment/item', {
+            title: result.name,
+            comment: result
+        });
+    });
+};
+//删除
+exports.del = function(req, res) {
+    if(!req.session.user) {
+        return res.render('server/message', {
+            msg: '请先登录'
+        });
+    }
+    var id = req.params.id;
+    Comment.findById(id).populate('author').exec(function(err, result) {
+        if(!result) {
+            return res.render('server/message', {
+                msg: '评论不存在'
+            });
+        }
+        console.log(result)
+        if(!result.author || result.author._id == req.session.user._id) {
+            result.remove(function(err) {
+                if(err) {
+                    return res.render('server/message', {
+                        msg: '删除失败222'
+                    });
+                }
+                res.render('server/message', {
+                    msg: '删除成功'
+                })
+            });
+        }else {
+            return res.render('server/message', {
+                msg: '你没有权限删除别人的评论'
+            });
+        }
+    });
+};
---
+++ server/routes/comment.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var util = require('../libs/util');
+var comment = require('../../server/controllers/comment');
+
+//权限判断
+router.use(function(req, res, next) {
+    console.log('评论页: ' + Date.now());
+    res.locals.Path = 'comment';
+    if(!req.session.user) {
+        var path = util.translateAdminDir('/user/login');
+        return res.redirect(path);
+    }
+    if(req.Roles && req.Roles.indexOf('admin') < 0 && req.Actions && req.Actions.indexOf('comment') < 0) {
+        var path = util.translateAdminDir('/index');
+        return res.redirect(path);
+    }
+    next();
+});
+//内容列表
+router.route('/').get(comment.list);
+//单条信息
+router.route('/:id').get(comment.one);
+//删除信息
+router.route('/:id/del').all(comment.del);
+
+module.exports = function(app) {
+    var path = util.translateAdminDir('/comment');
+    app.use(path, router);
+};
