3098f9ef00f042d2bc6537a1b5e4ccc90f7f1c0c
xiaoshan5733 <wengqianshan5733@gmail.com>
2014-07-03 00:08:53 +0800

    error handler

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('app/error', {
+        res.render('server/error', {
             message: err.message,
             error: err
         });
