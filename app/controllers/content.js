6430d2e422dd8970edd8630487807c29c88d10f3
xiaoshan5733 <wengqianshan5733@gmail.com>
2014-07-10 19:39:05 +0800

    comment style +1

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -36,6 +36,7 @@ exports.list = function(req, res) {
 //单条
 exports.one = function(req, res) {
     var id = req.param('id');
+    var nested = req.query.comment_nested;
     Content.findById(id).populate('author').populate('category').populate('comments').populate('gallery').exec(function(err, result) {
         //console.log(result);
         if(!result) {
@@ -49,7 +50,7 @@ exports.one = function(req, res) {
         res.render('app/content/item', {
             title: result.title,
             content: result,
-            comments: result.comments
+            comment_nested: nested
         });
     });
 };
