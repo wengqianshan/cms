453b73356da4c7e730960d5027e09caa26e59dee
xiaoshan5733 <wengqianshan5733@gmail.com>
2015-03-18 09:55:20 +0800

    Access-Control-Allow-Origin

--- routes/api/file.js
+++ routes/api/file.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 var file = require('../../controllers/api/file');
 
-//首页
+//
+router.use(function(req, res, next) {
+    console.log('api: ' + Date.now());
+    res.set({
+        'Access-Control-Allow-Origin': '*'
+    });
+    next();
+});
 router.route('/file').all(file.list);
 router.route('/file/:id').all(file.one);
 
