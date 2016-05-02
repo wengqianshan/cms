bbadf858827559f269478d2096e303a067677f80
xiaoshan5733 <wengqianshan5733@163.com>
2016-05-02 18:18:52 +0800

    service scheme

--- services/tag.js
+++ services/tag.js
@@ -8,3 +8,13 @@ var mongoose = require('mongoose'),
     config = require('../config'),
     core = require('../libs/core'),
     Tag = mongoose.model('Tag');
+
+/**
+- 查询
+  - 单个
+  - Id
+  - 批量
+- 添加
+- 更新
+- 删除
+ **/
