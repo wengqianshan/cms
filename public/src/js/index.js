b27757a90e84d9ad9bf7bc800d05e39efa666e31
前山 <wengqianshan5733@163.com>
2016-10-24 22:47:42 +0800

    clean

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,22 +1,14 @@
 require('../css/index.css');
 
-let users = ['xiaoshan', 'ruoguan'];
 import User from './user';
 console.log(User)
-
-users = users.map((user) => {
-    return user + '11'
-})
-
-console.log(users)
 
 class People {
 
     constructor(props) {
       this.name = 'ming'
     }
-
-
+    
     get() {
         console.log(this.name)
     }
