89c2ce59d01efee2a82cd6a43dd0aa1c59110469
xiaoshan5733 <wengqianshan5733@163.com>
2018-09-02 21:26:58 +0800

    file upload promise

--- services/file.js
+++ services/file.js
@@ -20,45 +20,24 @@ class Service extends Base {
   }
 
   upload(files, fields = {}) {
-    console.log('调试开始')
-    return new Promise((resolve, reject) => {
-      uploader.post(files, (result) => {
-        console.log('上传结果', result);
-
-        if (!result || !result.files) {
-          return reject({
-            code: -1,
-            message: '未知错误'
-          });
+    // console.log('调试开始')
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await uploader.post(files);
+        const result = [];
+        for (let i = 0; i < data.files.length; i ++) {
+          let item = data.files[i];
+          const fileObj = Object.assign(item, fields);
+          const file = await this.Model.create(fileObj);
+          result.push(file);
         }
-        let len = result.files.length;
-        let json = {
-          files: []
-        };
-        result.files.forEach((item) => {
-          Object.keys(fields).forEach((key) => {
-            item[key] = fields[key]
-          })
-          //这里还可以处理url
-          let fileObj = item; //_.pick(item, 'name', 'size', 'type', 'url');
-          // console.log('fileObj: ', fileObj);
-          let file = new this.Model(fileObj);
-          file.save((err, obj) => {
-            if (err || !obj) {
-              console.log('保存file失败', err, obj);
-              // return;
-            }
-            // TODO: 改成异步
-            len--;
-            item._id = obj._id;
-            json.files.push(item);
-            if (len === 0) {
-              // console.log(json)
-              resolve(json);
-            }
-          });
+        // console.log('上传结果: ', result);
+        resolve({
+          files: result
         });
-      });
+      } catch(e) {
+        reject(e);
+      }
     });
   }
 
@@ -67,22 +46,16 @@ class Service extends Base {
       const file = await this.Model.findById(id);
       try {
         // 删除主文件
-        uploader.delete(file.url, err => {
-          if (err) {
-            console.log(err)
-          }
-        });
+        await uploader.delete(file.url);
         // 删除封面
         if (file.covers && file.covers.length > 0) {
-          file.covers.forEach((cover) => {
-            uploader.delete(cover, err => {
-              console.log('删除封面: ', err);
-            })
-          })
+          file.covers.forEach(async (cover) => {
+            await uploader.delete(cover);
+          });
         }
         // 删除数据库记录
         file.remove((err, product) => {
-          console.log('文件数据库删除结果: ', err);
+          // console.log('文件数据库删除结果: ', err);
           if (err) {
             reject(err)
           } else {
@@ -90,8 +63,8 @@ class Service extends Base {
           }
         });
       } catch (e) {
-        console.log(e);
-        reject(e.message);
+        // console.log(e);
+        reject(e);
       }
     })
   }
